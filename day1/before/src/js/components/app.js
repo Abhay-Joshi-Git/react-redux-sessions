cf3d44816afb1cb579c62fe1b7d3cb22563a8009
abhay-joshi <abhay.joshi@drillinginfo.com>
2016-05-19 14:57:21 +0530

    initial changes

---
+++ day1/before/src/js/components/app.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default () => (
+    <div>
+
+        TODO - display classes list
+
+
+        <br />
+        <br />
+
+
+        TODO - display students list
+
+
+
+    </div>
+);
---
+++ day1/before/src/js/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/app.js";
+
+const students = [
+    {
+        name: 'John',
+        classId: 1
+    },
+    {
+        name: 'Jane',
+        classId: 2
+    },
+    {
+        name: 'Ronny',
+        classId: 2
+    },
+    {
+        name: 'Krish',
+        classId: 3
+    },
+    {
+        name: 'Bunty',
+        classId: 3
+    }
+];
+
+const classes = [
+    {
+        id: 1,
+        teacher : 'ABC'
+    },
+    {
+        id: 2,
+        teacher : 'XYZ'
+    },
+    {
+        id: 3,
+        teacher : 'PQR'
+    }
+];
+
+ReactDom.render(<App students={students} classes={classes}/>,
+    document.getElementById('app')
+);
